8a65696c2ea1d7a28634bca2555c6e875d11481a
Glenda Salas <glenda.salas.-nd@disney.com>
2022-01-12 13:10:30 -0600

    Add hot module to allow all kinds of modules to be updated at runtime without the need for a full refresh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import './App.css';
 
 const App = () => (
     <div className="App">
-        <h1> hello, world! </h1>
+        <h1> hello, world!!! </h1>
     </div>
 );
 
-export default App;
+export default hot(module)(App);
